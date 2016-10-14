b7a40b7c40ea82bbd5ecadb142e7d739fcde7796
julius59 <julius59@users.noreply.github.com>
2016-10-14 23:07:39 +0200

    Added more hidden files to remove

--- remove_stupid_files.sh
+++ remove_stupid_files.sh
@@ -14,12 +14,14 @@ fi
 echo "Removing Thumbs.db, .DS_Store etc..."
 # Removing whitespaces from files & folders name
 if [ $verbose -eq 1 ]; then
-	find $1 -type f \( -name "Thumbs.db" -or -name "desktop.ini" \
+	find $1 -type f \( -name "Thumbs.db" -or -name "desktop.ini" -or -name ".picasa.ini" \
 	-or -name ".DS_Store" -or -name "._DS_Store" -or -name "._.DS_Store" \) \
 	-print -delete
+	find $1 -type d -name ".dthumb" -print -delete
 else
-	find $1 -type f \( -name "Thumbs.db" -or -name "desktop.ini" \
+	find $1 -type f \( -name "Thumbs.db" -or -name "desktop.ini" -or -name ".picasa.ini" \
 	-or -name ".DS_Store" -or -name "._DS_Store" -or -name "._.DS_Store" \) \
 	-delete
+	find $1 -type d -name ".dthumb" -delete
 fi
 
